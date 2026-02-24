80d7ab5f8cee14398db20b7bea81c1d6c58fb2eb
unknown <chathuraprabashwara@gmail.com>
2026-02-24 19:29:29 +0530

    add cors

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.text({ limit: "10mb" }));
 
 let latestImage = null;
 
-app.use(bodyParser.text({ limit: "10mb" }));
-
-// Receive image from iPhone/React
+// POST endpoint to receive image
 app.post("/image", (req, res) => {
-    console.log("POST received!");
-    console.log("Body length:", req.body.length);
+    console.log("POST received, length:", req.body.length);
     latestImage = Buffer.from(req.body.split(",")[1], "base64");
     res.sendStatus(200);
 });
 
-// Serve the latest image
+// GET endpoint to serve the latest image
 app.get("/image.jpg", (req, res) => {
     if (!latestImage) return res.status(404).send("No image yet");
     res.writeHead(200, {
@@ -25,5 +26,5 @@ app.get("/image.jpg", (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000, () =>
-    console.log("Image server running")
+    console.log("Image server running on port 5000")
 );
