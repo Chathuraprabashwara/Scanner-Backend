58ef77d3c73aebc90b1ebea3060dd151c7aa2494
unknown <chathuraprabashwara@gmail.com>
2026-02-24 18:21:10 +0530

    adde initial project

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+let latestImage = null;
+
+app.use(bodyParser.text({ limit: "10mb" }));
+
+// Receive image from iPhone/React
+app.post("/image", (req, res) => {
+    latestImage = Buffer.from(req.body.split(",")[1], "base64");
+    res.sendStatus(200);
+});
+
+// Serve the latest image
+app.get("/image.jpg", (req, res) => {
+    if (!latestImage) return res.status(404).send("No image yet");
+    res.writeHead(200, {
+        "Content-Type": "image/jpeg",
+        "Content-Length": latestImage.length,
+    });
+    res.end(latestImage);
+});
+
+app.listen(process.env.PORT || 5000, () =>
+    console.log("Image server running")
+);
